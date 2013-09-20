acc33d262113867c49f4765c4f2edcf96e2b6007
Brandon <bcollins.audio@gmail.com>
2013-09-20 11:07:02 -0700

    add production settings file revert settings

--- api/settings_production.py
+++ api/settings_production.py
@@ -1,9 +1,28 @@
 from settings import *
 
+
 DEBUG = TEMPLATE_DEBUG = False
 
 SITE_ID = 1
 
 # cross domain xhr settings
-XS_SHARING_ALLOWED_ORIGINS = None
-XS_SHARING_ALLOWED_METHODS = None
+XS_SHARING_ALLOWED_ORIGINS = 'http://beta.levelskies.com'
+XS_SHARING_ALLOWED_METHODS = ['get','post']
+
+if host == live:
+    ADMINS = (
+        ('sys admin', 'sysadmin@levelskies.com'),
+    )
+
+    MANAGERS = ADMINS
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'api',
+            'USER': 'api',
+            'PASSWORD': 'buttwatercruises',
+            'HOST': '127.0.0.1',
+            'PORT': ''
+        }
+    }
