73366218c964579c345d4dfb131d820021e2651d
Ryan Houck <ryan@levelskies.com>
2014-04-12 16:24:00 -0700

    added alerts check to admin

--- sales/admin.py
+++ sales/admin.py
@@ -1,4 +1,4 @@
-from sales.models import Platform, Customer, Contract, Staging, Demo, Alerts
+from sales.models import Platform, Customer, Contract, Staging, Demo, Alerts, AlertsCheck
 
 from django.contrib import admin
 
@@ -102,3 +102,7 @@ admin.site.register(Staging, StagingAdmin)
 class AlertsAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Alerts, AlertsAdmin)
+
+class AlertsCheckAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(AlertsCheck, AlertsCheckAdmin)
