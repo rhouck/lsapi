aca6792ed96a0a52c866eb6839098a597ffd14da
Ryan Houck <ryan@levelskies.com>
2014-04-15 14:19:57 -0700

    update pruchase confirmation email

--- sales/views.py
+++ sales/views.py
@@ -509,7 +509,7 @@ def purchase_option(request):
                             else:
                                 ret = "on %s" % (find_search.return_date1.strftime("%B %d, %Y"))
 
-                            body = """You now have until %s to use your Flex Fare on a flight from %s to %s, leaving %s and returning %s.\n\nWhen you book your flight just forward the itinerary or confirmation email to sales@levelskies.com and we'll send you your payout if prices on the lowest fares have increased.\n\nThe Level Skies Team""" % (find_search.exp_date.strftime("%B %d, %Y"), find_search.origin_code, find_search.destination_code, dep, ret)
+                            body = """You have locked in today's low airfare of $%s. You now have until %s to use your Flex Fare when you book a flight from %s to %s, leaving %s and returning %s.\n\nWhen you book your flight just forward the itinerary or confirmation email to sales@levelskies.com and we'll send you your payout if prices on the lowest fares have increased.\n\nThe Level Skies Team""" % (int(find_search.locked_fare), find_search.exp_date.strftime("%B %d, %Y"), find_search.origin_code, find_search.destination_code, dep, ret)
 
                             send_template_email(new_contract.customer.email, subject, title, body)
                             
